8875a1191cccf8c36eba8d39dd9dfef5f36a7d99
fourglobe302500 <franciscomiguelolima@gmail.com>
2020-11-07 22:38:03 +0000

    Making an proper variable symbol

---
+++ Compiler/CodeAnalysis/Binding/BoundAssigmentExpression.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Compiler.CodeAnalysis.Binding
+{
+  internal class BoundAssigmentExpression : BoundExpression
+  {
+    public BoundAssigmentExpression(VariableSymbol variable, BoundExpression expression)
+    {
+      Variable = variable;
+      Expression = expression;
+    }
+
+    public override BoundNodeKind Kind => BoundNodeKind.AssigmentExpression;
+
+    public VariableSymbol Variable { get; }
+    public BoundExpression Expression { get; }
+    public override Type Type => Expression.Type;
+
+  }
+}
