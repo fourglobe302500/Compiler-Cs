35032ac1ef791b724bfef3d54e60bf3482ad0bea
fourglobe302500 <franciscomiguelolima@gmail.com>
2020-11-11 02:32:36 +0000

    Major Changes In The Tests Folder
    minor life quality fixes in tasks.json and launch.json

--- Compiler/CodeAnalysis/Syntax/SyntaxTree.cs
+++ Compiler/CodeAnalysis/Syntax/SyntaxTree.cs
@@ -3,24 +3,34 @@ using System.Linq;
 
 namespace Compiler.CodeAnalysis.Syntax
 {
-  public sealed class SyntaxTree
-  {
-    public SyntaxTree(
-      IEnumerable<Diagnostic> diagnostics,
-      ExpressionSyntax root,
-      SyntaxToken endOfFileToken)
+    public sealed class SyntaxTree
     {
-      Diagnostics = diagnostics.ToArray();
-      Root = root;
-      EndOfFileToken = endOfFileToken;
+        public SyntaxTree(
+            IEnumerable<Diagnostic> diagnostics,
+            ExpressionSyntax root,
+            SyntaxToken endOfFileToken)
+        {
+            Diagnostics = diagnostics.ToArray();
+            Root = root;
+            EndOfFileToken = endOfFileToken;
+        }
+
+        public IReadOnlyList<Diagnostic> Diagnostics { get; }
+        public ExpressionSyntax Root { get; }
+        public SyntaxToken EndOfFileToken { get; }
+
+        public static SyntaxTree Parse(string text) => 
+            new Parser(text).Parse;
+        public static IEnumerable<SyntaxToken> ParseTokens(string text)
+        {
+            var lexer = new Lexer(text);
+            while (true)
+            {
+                var token = lexer.Lex();
+                if (token.Kind == SyntaxKind.EndOfFileToken)
+                    break;
+                yield return token;
+            }
+        }
     }
-
-    public IReadOnlyList<Diagnostic> Diagnostics { get; }
-    public ExpressionSyntax Root { get; }
-    public SyntaxToken EndOfFileToken { get; }
-
-    public static SyntaxTree Parse(string text) => 
-      new Parser(text).Parse;
-  }
-
 }
