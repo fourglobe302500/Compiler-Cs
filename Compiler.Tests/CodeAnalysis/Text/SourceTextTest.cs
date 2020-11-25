d540354dc72427b9c4436e8c1465afeb2145d75c
fourglobe302500 <franciscomiguelolima@gmail.com>
2020-11-25 21:09:07 +0000

    Adding the test for SourceText

---
+++ Compiler.Tests/CodeAnalysis/Text/SourceTextTest.cs
@@ -0,0 +1,18 @@
+﻿using Compiler.CodeAnalysis.Text;
+using Xunit;
+
+namespace Compiler.Tests.CodeAnalysis.Text
+{
+    public class SourceTextTest
+    {
+        [Theory]
+        [InlineData(".", 1)]
+        [InlineData(".\r\n", 2)]
+        [InlineData(".\r\n\r\n", 3)]
+        public void SourceText_IncludesLastLine(string text, int numberOfLines)
+        {
+            SourceText sourceText = SourceText.From(text);
+            Assert.Equal(sourceText.Lines.Length, numberOfLines);
+        }
+    }
+}
